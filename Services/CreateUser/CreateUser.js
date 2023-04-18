882e1d53da2719abc8e05473fa7abcc424d4aaf4
Gabriel Fernandes <gfernandes410@gmail.com>
2023-04-17 23:36:21 -0300

    added create user route

--- Services/CreateUser/CreateUser.js
+++ Services/CreateUser/CreateUser.js
@@ -1,13 +1,39 @@
-
+import Email from '../../Objects/EmailAddress.js';
+import IdFactory from '../../Factories/Id/index.js';
+import Password from '../../Objects/Password.js';
+import resolveModel from '../../Helpers/resolveModel.js';
 import User from '../../Entities/User/index.js';
-
 export default class CreateUser {
 
+    #conn;
+
+    constructor() {
+        this.#conn = resolveModel('users');
+    }
+
     /**
-     * @param {{ body: import("../../Entities/User/Properties.js").default; }} request
+     * @param {any} request // TO-DO implement type
      */
     async execute(request) {
-        new User(request.body);
+        const user = this._createUser(request);
+
+        this.#conn.insertOne(user.toObject());
+
+        return user.toObject();
+    }
+
+    /**
+     * @param {any} request // TO-DO implement type
+     * @return {User}
+     */
+    _createUser(request) {
+        return new User({
+            id:  new IdFactory().create(),
+            email: new Email(request.body.email),
+            firstName: request.body.firstName,
+            lastName: request.body.lastName,
+            password: new Password(request.body.passoword)
+        });
     }
 
 }
--- http/routes/public/User.js
+++ http/routes/public/User.js
@@ -1,36 +1,17 @@
-import Email from '../../../Objects/EmailAddress.js';
+
 import express from 'express';
-import Password from '../../../Objects/Password.js';
-import User from '../../../Entities/User/index.js';
-import resolveModel from '../../../Helpers/resolveModel.js';
-import IdFactory from '../../../Factories/Id/index.js';
+
 import CreateUserController from '../../Controller/User/Create.js';
 
 const router = express.Router();
 
 router.get('/ping',(request, response) => {
     console.log('Pong!')
-    response.send('Pon22g!');
+    response.send('Pong!');
 })
 
 router.post('',(request, response) => {
-
-    new CreateUserController().handle(request)
-
-    const user = new User({
-        id:  new IdFactory().create(),
-        email: new Email('new user@example.com'),
-        firstName: 'dsasad',
-        lastName: 'dsasad',
-        password: new Password('1321')
-    });
-   
-    const con = resolveModel('users');
-
-    con.insertOne(user.toObject());
-
-    console.log('created user!')
-    response.send('Pong!');
+    new CreateUserController().handle(request);
 })
 
 export default router;
